549fa55d29a4faee9647ebd7095df66ee67f1c77
George Pauer <gpauer@student.wethinkcode.co.za>
2020-02-26 00:39:36 +0200

    ft_putnbr correction

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include "stdio.h"
 
 void	ft_putnbr(int n)
 {
@@ -7,15 +8,15 @@ void	ft_putnbr(int n)
 	if (n < 0)
 	{
 		ft_putchar('-');
-		n = (n * -1);
+		n *= -1;
 	}
-	if (n > 0)
+	if (n < 10 && n >= 0)
+		ft_putchar(n + '0');
+	else if (n >= 0)
 	{
 		m = n % 10;
 		n = n / 10;
 		ft_putnbr(n);
 		ft_putchar(m + '0');
 	}
-	else
-		ft_putchar('0');
 }
